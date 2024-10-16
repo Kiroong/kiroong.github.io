dad65d9b2a9982742fcad472351aa73cbeab405d
javelinsman <javelinsman@snu.ac.kr>
2024-10-16 19:02:48 +0900

    VIS 2024 version

--- intro-chi-2024/src/App.tsx
+++ intro-chi-2024/src/App.tsx
@@ -11,7 +11,7 @@ import ImageLWInterface from "./figures/lw-interface.png"
 import ImageLWResult from "./figures/lw-result.png"
 import ImageP101Interface from "./figures/p101-interface.png"
 import ImageNewIdea from "./figures/new-idea.png"
-import { IoMailOutline } from "react-icons/io5"
+import { IoDocumentTextOutline, IoMailOutline } from "react-icons/io5"
 
 const App = () => {
   return (
@@ -27,6 +27,61 @@ const App = () => {
             </li>
             <li className="flex items-center justify-start gap-3 -ml-1.5">
               <IoMailOutline /> krchoe@hcil.snu.ac.kr
+            </li>
+          </ul>
+        </div>
+
+        {/* Quick Links for VIS 2024 Section */}
+        <div className="bg-white shadow rounded-lg p-6 mb-6">
+          <h2 className="text-xl font-semibold mb-4">
+            Papers Presented at VIS 2024
+          </h2>
+          <ul className="space-y-2">
+            <li className="flex items-center">
+              <a
+                href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/197/original/Submission_Manuscript_R1.pdf?1718443507"
+                className="flex items-center w-full hover:bg-gray-100 p-2 rounded-lg"
+              >
+                <div className="flex flex-col items-center justify-center w-8 mr-3">
+                  <div className="flex items-center justify-center w-8 h-8 bg-red-500 rounded-lg mb-1">
+                    <IoDocumentTextOutline className="text-white text-xl" />
+                  </div>
+                  <span className="text-xs">PDF</span>
+                </div>
+                <div>
+                  <span className="text-blue-600 hover:underline mr-2">
+                    Enhancing Data Literacy On-demand: LLMs as Guides for
+                    Novices in Chart Interpretation
+                  </span>
+                  <p className="text-sm text-gray-600">
+                    Kiroong Choe, Chaerin Lee, Soohyun Lee, Jiwon Song, Aeri Cho,
+                    Nam Wook Kim, Jinwook Seo (VIS 2024 Full Paper; TVCG)
+                  </p>
+                </div>
+              </a>
+            </li>
+            <li className="flex items-center mt-4">
+              <a
+                href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/198/original/vis24b-sub1173-cam-i7_%283%29.pdf?1726028942"
+                className="flex items-center w-full hover:bg-gray-100 p-2 rounded-lg"
+              >
+                <div className="flex flex-col items-center justify-center w-8 mr-3">
+                  <div className="flex items-center justify-center w-8 h-8 bg-red-500 rounded-lg mb-1">
+                    <IoDocumentTextOutline className="text-white text-xl" />
+                  </div>
+                  <span className="text-xs">PDF</span>
+                </div>
+                <div>
+                  <span className="text-blue-600 hover:underline mr-2">
+                    Fields, Bridges, and Foundations: How Researchers Browse
+                    Citation Network Visualizations
+                  </span>
+                  <p className="text-sm text-gray-600">
+                    Kiroong Choe, Eunhye Kim, Sangwon Park, Jinwook Seo (VIS 2024
+                    Short Paper)
+                  </p>
+                </div>
+              </a>
             </li>
           </ul>
         </div>
@@ -50,6 +105,60 @@ const App = () => {
           </ul>
         </div>
 
+        {/* Complex Charts Section */}
+        <div className="bg-white shadow rounded-lg p-6 mb-6">
+          <h2 className="text-xl font-semibold mb-4">
+            <span className="underline">Complex Charts</span>, made
+            understandable
+          </h2>
+          <h3 className="text-md font-semibold mt-4">
+            <a href="https://ieeexplore.ieee.org/document/10555321">
+              Enhancing Data Literacy On-demand: LLMs as Guides for Novices in
+              Chart Interpretation
+            </a>{" "}
+            <a href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/197/original/Submission_Manuscript_R1.pdf?1718443507">
+              [PDF]
+            </a>
+          </h3>
+          <p className="text-gray-600 text-sm">
+            Kiroong Choe,{" "}
+            <span className="text-gray-400">
+              Chaerin Lee, Soohyun Lee, Jiwon Song, Aeri Cho, Nam Wook Kim,
+              Jinwook Seo
+            </span>{" "}
+            (TVCG; presented at VIS 2024)
+          </p>
+          <div className="py-6 border-b-2">
+            <p className="text-sm">
+              The general public increasingly encounters complex charts, but
+              their primary resource for understanding these is often just
+              Google.
+            </p>
+            <img
+              src={ImageClvpExample}
+              className="w-full max-w-[400px] mx-auto"
+            />
+          </div>
+          <div className="py-6 border-b-2">
+            <p className="text-sm pb-2">
+              We have facilitated chart discussions using LLM through both
+              textual and visual annotations.
+            </p>
+            <img src={ImageClvpInterface} className="w-full" />
+          </div>
+          <div className="py-6">
+            <p className="text-sm pb-2">
+              LLM and visual communication effectively onboarded people to
+              complex visualizations but may also posed risks, such as creating
+              performance illusions and reducing engagement.
+            </p>
+            <img
+              src={ImageClvpBarcharts}
+              className="w-full max-w-[480px] mx-auto"
+            />
+          </div>
+        </div>
+
         {/* Citation Networks Section */}
         <div className="bg-white shadow rounded-lg p-6 mb-6">
           {/* <h2 className="text-xl font-semibold mb-4">Citation Networks</h2> */}
@@ -59,8 +168,13 @@ const App = () => {
           </h2>
           {/* <p>I make citation networks manually browsable.</p> */}
           <h3 className="text-md font-semibold mt-4">
-            Fields, Bridges, and Foundations: How Researchers Browse Citation
-            Network Visualizations
+            <a href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/198/original/vis24b-sub1173-cam-i7_%283%29.pdf?1726028942">
+              Fields, Bridges, and Foundations: How Researchers Browse Citation
+              Network Visualizations
+            </a>{" "}
+            <a href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/198/original/vis24b-sub1173-cam-i7_%283%29.pdf?1726028942">
+              [PDF]
+            </a>
           </h3>
           <p className="text-gray-600 text-sm">
             Kiroong Choe,{" "}
@@ -112,64 +226,17 @@ const App = () => {
           <h3 className="text-lg font-semibold mt-4">
             System for Manually Browsing Citation Networks
           </h3>
-          <p className="text-gray-600 text-sm">(In development for CHI 2025)</p>
+          <p className="text-gray-600 text-sm">
+            (In development for UIST 2025)
+          </p>
           <div className="py-6">
             <p className="text-sm">
               I believe that creating a system to manually browse citation
               networks will synergize with emerging intelligent search tools.
               I'm working on a system that utilizes graph simplification and an
-              AI compass.
+              LLM as a compass.
             </p>
             <img src={ImageNewIdea} className="w-full" />
-          </div>
-        </div>
-
-        {/* Complex Charts Section */}
-        <div className="bg-white shadow rounded-lg p-6 mb-6">
-          <h2 className="text-xl font-semibold mb-4">
-            <span className="underline">Complex Charts</span>, made
-            understandable
-          </h2>
-          <h3 className="text-md font-semibold mt-4">
-            <a href="https://ieeexplore.ieee.org/document/10555321">Enhancing Data Literacy On-demand: LLMs as Guides for Novices in
-            Chart Interpretation</a>
-          </h3>
-          <p className="text-gray-600 text-sm">
-            Kiroong Choe,{" "}
-            <span className="text-gray-400">
-              Chaerin Lee, Soohyun Lee, Jiwon Song, Aeri Cho, Nam Wook Kim,
-              Jinwook Seo
-            </span>{" "}
-            (TVCG; to be presented at VIS 2024)
-          </p>
-          <div className="py-6 border-b-2">
-            <p className="text-sm">
-              The general public increasingly encounters complex charts, but
-              their primary resource for understanding these is often just
-              Google.
-            </p>
-            <img
-              src={ImageClvpExample}
-              className="w-full max-w-[400px] mx-auto"
-            />
-          </div>
-          <div className="py-6 border-b-2">
-            <p className="text-sm pb-2">
-              We have facilitated chart discussions using LLM through both
-              textual and visual annotations.
-            </p>
-            <img src={ImageClvpInterface} className="w-full" />
-          </div>
-          <div className="py-6">
-            <p className="text-sm pb-2">
-              LLM and visual communication effectively onboarded people to
-              complex visualizations but may also posed risks, such as creating
-              performance illusions and reducing engagement.
-            </p>
-            <img
-              src={ImageClvpBarcharts}
-              className="w-full max-w-[480px] mx-auto"
-            />
           </div>
         </div>
 
@@ -180,28 +247,13 @@ const App = () => {
           </h2>
 
           <h3 className="text-md font-semibold mt-4">
-            Papers101: Supporting the discovery process in the literature review
-            workflow for novice researchers
-          </h3>
-          <p className="text-gray-600 text-sm">
-            Kiroong Choe,{" "}
-            <span className="text-gray-400">
-              Seokhyeon Park, Seokweon Jung, Hwajung Hong, Jinwook Seo
-            </span>{" "}
-            (PacificVis'21)
-          </p>
-          <div className="mt-4 pb-6 border-b-2">
-            <p className="text-sm pb-2">
-              Searching on Google Scholar often results in numerous disorganized
-              outcomes. We designed a unified visualization that ranks relevant
-              papers.
-            </p>
-            <img src={ImageP101Interface} className="w-full" />
-          </div>
-
-          <h3 className="text-md font-semibold mt-12">
-            Supporting Novice Researchers to Write Literature Review using
-            Language Models
+            <a href="https://dl.acm.org/doi/10.1145/3613905.3650787">
+              Supporting Novice Researchers to Write Literature Review using
+              Language Models
+            </a>{" "}
+            <a href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/193/original/3613905.3650787.pdf?1718209740">
+              [PDF]
+            </a>
           </h3>
           <p className="text-gray-600 text-sm">
             Kiroong Choe,{" "}
@@ -229,13 +281,38 @@ const App = () => {
             </p>
             <img src={ImageLWInterface} className="w-full" />
           </div>
-          <div className="py-6">
+          <div className="py-6 border-b-2">
             <p className="text-sm mb-2">
               LLMs not only helped initiate and develop narratives but also
               acted as supportive companions, boosting confidence and fostering
               a sense of agency.
             </p>
             <img src={ImageLWResult} className="w-full max-w-[350px] mx-auto" />
+          </div>
+
+          <h3 className="text-md font-semibold mt-12">
+            <a href="https://ieeexplore.ieee.org/abstract/document/9438793">
+              Papers101: Supporting the discovery process in the literature
+              review workflow for novice researchers
+            </a>{" "}
+            <a href="http://hcil.snu.ac.kr/system/publications/pdfs/000/000/150/original/pacificvis21c-sub1033-cam-i5_%283%29.pdf?1618301220">
+              [PDF]
+            </a>
+          </h3>
+          <p className="text-gray-600 text-sm">
+            Kiroong Choe,{" "}
+            <span className="text-gray-400">
+              Seokhyeon Park, Seokweon Jung, Hwajung Hong, Jinwook Seo
+            </span>{" "}
+            (PacificVis'21)
+          </p>
+          <div className="mt-4 pb-6 border-b-2">
+            <p className="text-sm pb-2">
+              Searching on Google Scholar often results in numerous disorganized
+              outcomes. We designed a unified visualization that ranks relevant
+              papers.
+            </p>
+            <img src={ImageP101Interface} className="w-full" />
           </div>
         </div>
 
